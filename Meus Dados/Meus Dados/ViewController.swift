7363065f13c78dbb07167fd8f3ab1264865843ba
Vitor Souza <64599506+vitor-csouza@users.noreply.github.com>
2024-02-26 22:37:27 -0300

    adicionando appIcon

--- Meus Dados/Meus Dados/ViewController.swift
+++ Meus Dados/Meus Dados/ViewController.swift
@@ -2,27 +2,32 @@
 //  ViewController.swift
 //  Meus Dados
 //
-//  Created by Vitor Souza on 22/02/24.
+//  Created by Usuário Convidado on 22/02/24.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+   
     @IBOutlet weak var lbNome: UILabel!
     @IBOutlet weak var lbCidade: UILabel!
-    
+    @IBOutlet weak var lbIdade: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         lbNome.text="Seu nome é..."
-        lbCidade.text="Sua cidade é..."
+        lbCidade.text="Sua cidade é...."
+        lbIdade.text="Sua idade é..."
     }
 
     @IBAction func Exibir(_ sender: Any) {
-        lbNome.text="Vitor"
+        lbNome.text="Vitor Souza"
         lbCidade.text="São Paulo"
+        lbIdade.text="22 anos"
     }
     
+    @IBAction func Limpar(_ sender: Any) {
+        viewDidLoad()
+    }
 }
 
